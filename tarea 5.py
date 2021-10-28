fb39245b3cde8183017fd73585160269db95b9df
Barroso03 <91721590+Barroso03@users.noreply.github.com>
2021-10-28 20:27:24 +0200

    creo que voy bien en el 5

---
+++ tarea 5.py
@@ -0,0 +1,10 @@
+hambre_helado_1 = 17
+hambre_helado_1 = hambre_helado_1
+print(hambre_helado_1)
+dinero_helado_1 = 2000
+dinero_helado_1 = dinero_helado_1 - 120
+print(dinero_helado_1)
+if hambre_helado_1 == 17 and dinero_helado_1 == 1880 :
+    print("Tengo hambre")
+
+ 

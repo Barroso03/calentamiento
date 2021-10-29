2db8dc060a053368b619d75e378bcf299856dfc8
Barroso03 <91721590+Barroso03@users.noreply.github.com>
2021-10-29 19:01:13 +0200

    Merge branch 'main' of https://github.com/Barroso03/calentamiento

--- tarea 5.py
+++ tarea 5.py
@@ -1,10 +1,18 @@
-<<<<<<< HEAD
-hambre = 17
-precio_helado = 100
+hungry = 17 or 34 or 51 or 68 or 85
+money = 2000
+if money == 2000 or money >= 1700:
+ print("money is either 2000 or above 1700")
+money == 2000 or money >= 1700 and  hungry = 17 or 34 or 51 or 68
+if money == 2000 or money >= 1700 and  hungry == 17 or 34 or 51 or 68:
+ print("I am hungry")
+else if money <= 1700 and not hungry:
+ print("I am not hungry")
+else: # If all other if conditions are not met
+ print("Not sure who this is or if they are hungry")
     
-=======
+
 
  
 
  
->>>>>>> 24b46b318850bcf159504e09883a03cd005f08bf
+

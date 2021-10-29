24b46b318850bcf159504e09883a03cd005f08bf
Barroso03 <91721590+Barroso03@users.noreply.github.com>
2021-10-29 08:57:39 +0200

    hasta tarea 6

--- tarea 5.py
+++ tarea 5.py
@@ -1,11 +1,4 @@
-hambre_1 = 17
-hambre_1 = hambre_1
-print(hambre_1)
-dinero_1 = 2000
-dinero_1 = dinero_1 - 120
-print(dinero_1)
-if hambre_1 == 17 and dinero_1 == 1880 :
-    print("Tengo hambre")
+
  
 
  

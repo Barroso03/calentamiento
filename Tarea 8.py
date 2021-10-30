27c3f080f1e45fb60b374511ab4f4041163269df
Barroso03 <91721590+Barroso03@users.noreply.github.com>
2021-10-30 12:58:06 +0200

    tarea7

---
+++ Tarea 8.py
@@ -0,0 +1,7 @@
+
+def bitcoinToeuros(bitcoint_amount, euros_value):
+    bitcoin_amount = 1.2
+    euros_value = 25000
+    bitcoinToeuros = bitcoin_amount * euros_value
+    print(bitcoinToeuros) 
+ 

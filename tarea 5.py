4f28a5269d2b6ee90c1d86b0bd2361e28540dd88
Barroso03 <91721590+Barroso03@users.noreply.github.com>
2021-10-30 11:49:36 +0200

    cambios 2

--- tarea 5.py
+++ tarea 5.py
@@ -1,11 +1,12 @@
-hungry = 17 or 34 or 51 or 68 or 85
+hungry = 17, 34, 51, 68
+not hungry == 85
 money = 2000
-if money == 2000 or money >= 1700:
+if money <= 2000 or money >= 1700:
  print("money is either 2000 or above 1700")
-money == 2000 or money >= 1700 and  hungry = 17 or 34 or 51 or 68
-if money == 2000 or money >= 1700 and  hungry == 17 or 34 or 51 or 68:
+money <= 2000 or money >= 1700 and hungry = 17, 34, 51, 68
+if money <= 2000 or money >= 1700 and  hungry == [17, 34, 51, 68]:
  print("I am hungry")
-else if money <= 1700 and not hungry:
+elif money <= 1700 and not hungry:
  print("I am not hungry")
 else: # If all other if conditions are not met
  print("Not sure who this is or if they are hungry")
